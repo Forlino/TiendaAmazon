c9794bbcfb6bcec284c467b254ec217163593841
unknown <Lucasforlino2001@gmailcom>
2025-04-15 18:13:55 -0300

    Corregir rutas y archivos para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TiendaAmazon/',  // Especifica el nombre del repositorio donde se alojará el proyecto
   plugins: [react()],
 })
